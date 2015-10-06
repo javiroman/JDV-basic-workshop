6e5773646ff7d744a8aecba734e7a68305a3ae8b
Javi Roman <jroman.espinar@gmail.com>
2015-10-06 20:30:25 +0200

    note about upstream JDBC driver in AS

--- simpleclients/java/run.sh
+++ simpleclients/java/run.sh
@@ -1,4 +1,11 @@
 mvn -q install -Dvdb="myVDB" -Dsql="select * from carshop" -Dusername="user" -Dpassword="@123user"
 
+#
 # Testing Dynamic VDB
+#
+
+# Simple SQL select:
 # mvn install -Dvdb="CarSalesDatabase" -Dsql="select * from carshop" -Dusername="user" -Dpassword="@123user"
+
+# SQL join:
+# mvn -q install -Dvdb="CarSalesDatabase" -Dsql="SELECT carID,models.* FROM shopModels LEFT JOIN models ON models.modID = shopModels.modID;" -Dusername="user" -Dpassword="@123user"
